f35969e7b1a32b9e34a707ce763aa91c078b9ad3
lindenbergue10 <104520059+lindenbergue10@users.noreply.github.com>
2022-06-23 10:40:56 -0300

    Create q11.py

---
+++ q11.py
@@ -0,0 +1,7 @@
+v = int(input('Digite um número: '))
+if v>80:
+    print('Mensagem mostrada, Maior que 80')
+elif v<25:
+    print('Mensagem mostrada, Menor que 25')
+elif v==40:
+    print('Mensagem mostrada, Igual a 40')
