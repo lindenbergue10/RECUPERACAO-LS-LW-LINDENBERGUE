4f9e9c8ca29fa86f98d89af2efdb6ed6eacd4d92
lindenbergue10 <104520059+lindenbergue10@users.noreply.github.com>
2022-06-23 10:47:06 -0300

    Create q4.py

---
+++ q4.py
@@ -0,0 +1,15 @@
+a = float(input())
+
+b = float(input())
+
+c = float(input())
+
+media=(a+b+c)/3
+
+
+if media>= 7:
+    print("aprovado")
+elif media<=5:
+    print("reprovado")
+else: 
+    print("Recuperação")
